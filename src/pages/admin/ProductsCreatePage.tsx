df8abc7b94ac9d6c48ae08fa5e915ec85be4e37a
truongpham1309 <truong13092002@gmail.com>
2023-11-26 19:32:45 +0700

    addproducts and update products

--- src/pages/admin/ProductsCreatePage.tsx
+++ src/pages/admin/ProductsCreatePage.tsx
@@ -1,14 +1,60 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import { Product, initalProduct } from "../../types/products";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProductsCreatePage = () => {
     const [categories, setCategories] = useState<string[]>([]);
+    const [productAdd, setProductAdd] = useState<Product>(initalProduct);
+
+    const handleChangeInfoProductAdd = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        setProductAdd({ ...productAdd, [e.currentTarget.name]: e.currentTarget.value });
+        console.log(productAdd);
+    }
+
+    const handleSubmitFormAddProduct = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        let valid = false;
+        if (productAdd.title.trim().length === 0) {
+            valid = true;
+            toast.error("Title is required! Please try again");
+        }
+        if (productAdd.price <= 0) {
+            valid = true;
+            toast.error("Price cannot be less than or equal to 0!");
+        }
+        if (productAdd.category.trim().length === 0) {
+            valid = true;
+            toast.error("Category cannot be empty!");
+        }
+        if (productAdd.description.trim().length === 0) {
+            valid = true;
+            toast.error("Description cannot be empty!");
+        }
+
+        if (valid) return;
+
+        toast.promise((async () => {
+            try {
+                const { data } = await axios.post("https://fakestoreapi.com/products", {...productAdd});
+                console.log(data);
+                return;
+            } catch (error) {
+                console.log(error);
+            }
+        })(),{
+            pending: "Creating...",
+            success: "Create Successfully ✅",
+            error: "Create Failed ❌",
+        })
+    }
 
 
     useEffect(() => {
         (async () => {
             try {
-                const { data: categories} = await axios.get("https://fakestoreapi.com/products/categories");
+                const { data: categories } = await axios.get("https://fakestoreapi.com/products/categories");
                 setCategories(categories);
             } catch (error) {
                 console.log(error);
@@ -21,11 +67,12 @@ const ProductsCreatePage = () => {
             {/* component */}
             <div className="flex justify-center items-center w-full bg-white">
                 {/* COMPONENT CODE */}
+                <ToastContainer />
                 <div className="container mx-auto my-4 px-4 lg:px-20">
-                    <form action="">
+                    <form action="" onSubmit={handleSubmitFormAddProduct}>
                         <div className="w-full p-8 my-4 md:px-12 lg:w-full lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
                             <div className="flex">
-                                <h1 className="font-bold uppercase text-5xl">
+                                <h1 className="font-bold uppercase text-3xl">
                                     Create a <br /> products
                                 </h1>
                             </div>
@@ -33,19 +80,26 @@ const ProductsCreatePage = () => {
                                 <input
                                     className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                     type="text"
+                                    value={productAdd.title}
+                                    name="title"
+                                    onChange={handleChangeInfoProductAdd}
                                     placeholder="Title*"
                                 />
                                 <input
                                     className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                                    type="text"
+                                    type="number"
+                                    name="price"
+                                    onChange={handleChangeInfoProductAdd}
+                                    value={productAdd.price}
                                     placeholder="Price*"
                                 />
                                 <input
                                     className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none"
                                     type="file"
                                     placeholder="Image*"
+                                    onChange={handleChangeInfoProductAdd}
                                 />
-                                <select placeholder="Category" className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" name="" id="">
+                                <select placeholder="Category" onChange={handleChangeInfoProductAdd} className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" name="category" id="">
                                     <option value="">Category</option>
                                     {categories.map((cate, index) => (
                                         <option key={index} value={cate}>{cate}</option>
@@ -55,12 +109,15 @@ const ProductsCreatePage = () => {
                             <div className="my-4">
                                 <textarea
                                     placeholder="Description*"
+                                    name="description"
+                                    onChange={handleChangeInfoProductAdd}
                                     className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                     defaultValue={""}
                                 />
                             </div>
                             <div className="my-2 w-1/2 lg:w-1/4">
                                 <button
+                                    type="submit"
                                     className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline"
                                 >
                                     Create
--- src/types/products/index.ts
+++ src/types/products/index.ts
@@ -11,3 +11,16 @@ export type Product = {
     category: string,
     rating: ProductRate
 }
+ 
+export const initalProduct: Product = {
+    id: 0,
+    title: "",
+    image: "",
+    price: 0,
+    description: "",
+    category: "",
+    rating: {
+      rate: 0,
+      count: 0,
+    }
+  }
